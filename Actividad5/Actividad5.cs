87bc13a9cd97aa310e82c813961fcd40063b035b
Javier Rodríguez <frodsot2402@g.educaand.es>
2024-10-14 14:32:40 +0200

    Agregar archivos de proyecto.

---
+++ Actividad5/Actividad5.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace Actividad5
+{
+    public class Actividad5
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string devolverString(string[] vs)
+        {
+            //TODO
+
+            throw new NotImplementedException();
+        }
+
+        public static string devolverStringOrdenado(string[] vs)
+        {
+
+            //TODO
+
+            throw new NotImplementedException();
+        }
+
+        public static int contarCaracter(string[] vs, char v)
+        {
+            //TODO
+
+            throw new NotImplementedException();
+        }
+
+        public static int[] sumarArrays(int[] vs1, int[] vs2)
+        {
+
+            //TODO
+
+            throw new NotImplementedException();
+        }
+
+        public static int[] multiplicarArrays(int[] vs1, int[] vs2)
+        {
+            //TODO
+
+            throw new NotImplementedException();
+        }
+
+        public static bool isPalindromo(string entrada)
+        {
+            //TODO
+
+            throw new NotImplementedException();
+
+
+        }
+
+        public static int contarPalabras(string entrada)
+        {
+            //TODO
+
+            throw new NotImplementedException();
+        }
+
+        public static int contarCaracteres(string entrada)
+        {
+            //TODO
+
+            throw new NotImplementedException();
+        }
+    }
+}
